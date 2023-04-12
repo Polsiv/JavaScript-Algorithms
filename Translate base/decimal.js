c12ecadc387d1299d0816fd276818f5c654f4be8
Silv <91438536+Polsiv@users.noreply.github.com>
2023-04-12 10:59:56 -0500

    Commiting new files

---
+++ Translate base/decimal.js
@@ -0,0 +1,17 @@
+
+ let Number = prompt("Enter the number")
+ let Base = prompt("Enter the base")
+
+
+
+function convertToDecimal(number, base) {
+    let decimal = 0;
+
+for( let i = 0; i < number.length; i++){
+let digit = parseInt(number[i], base)
+}
+    
+console.log(decimal)
+  }
+
+  convertToDecimal(Number, Base)
---
+++ Translate base/main.js
@@ -0,0 +1,6 @@
+let base = prompt("Enter the base")
+let number = prompt("enter the number");
+
+let numsolution = parseInt(number).toString(base);
+
+console.log("the number " + number + " In base: " + base + ", is equal to: " + numsolution)
